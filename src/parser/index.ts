60b215b87787620132fca97276d57ca261a17cac
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-07-30 10:35:13 +0800

    Fix parser to collect whitespace properly

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,17 +1,45 @@
-import { alternation, parse, quantifier } from './core';
+import {
+  alternation,
+  parse,
+  quantifier,
+  sequence,
+} from './core';
+import blank from './rules/blank';
 import atom, { Atom } from './rules/atom';
 import atomWithVariant, { AtomWithVariant } from './rules/atom-with-variant';
 
 export default function parseClassnames(
   classnames: string,
 ): (Atom | AtomWithVariant)[] | undefined {
-  const list = quantifier(
+  const list = sequence(
     alternation(
       atom,
       atomWithVariant,
     ),
+    quantifier(
+      sequence(
+        blank,
+        alternation(
+          atom,
+          atomWithVariant,
+        ),
+      ),
+    ),
   );
 
   const result = parse(list, classnames);
-  return result?.value as (Atom | AtomWithVariant)[] | undefined;
+  if (result) {
+    const mappers = [result.value[0] as (Atom | AtomWithVariant)];
+
+    if (result.value[1]) {
+      const item = result.value[1];
+      if (Array.isArray(item.value)) {
+        for (const mapper of item.value) {
+          mappers.push(mapper.value[1] as (Atom | AtomWithVariant));
+        }
+      }
+    }
+    return mappers;
+  }
+  return undefined;
 }
